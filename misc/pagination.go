a2db895a5a89815783d1ba6689f57e451a1783f8
chengzi <chengzi@kwin.im>
2021-06-03 14:25:04 +0800

    update

--- misc/pagination.go
+++ misc/pagination.go
@@ -1,13 +1,27 @@
 package misc
 
+import (
+	"regexp"
+)
+
 const defaultLimit = 10
+
+var re, _ = regexp.Compile(`(?i:^select (.+?) from) *`)
+var countStatement = []byte("select count(1) from ")
 
 type Pagination struct {
 	Limit  int `json:"limit"`
 	Offset int `json:"offset"`
 	Page   int `json:"page"`
 	Total  int `json:"total"`
+	Sql    string
 }
+
+func (p Pagination) CountSql(sql string) string {
+	rs := re.ReplaceAll([]byte(sql), countStatement)
+	return string(rs)
+}
+
 
 func NewPagination(page, limit int) Pagination {
 	if page < 1 {
