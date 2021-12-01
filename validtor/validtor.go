c34a710dfffb95b59371170b880fe0712254a25a
chengzi <chengzi@kwin.im>
2021-12-01 17:09:10 +0800

    update

---
+++ validtor/validtor.go
@@ -0,0 +1,71 @@
+package validtor
+
+import (
+	"fmt"
+	"regexp"
+	"unicode"
+)
+
+const ()
+
+var (
+	reMail, _     = regexp.Compile(RegexMail)
+	reUserName, _ = regexp.Compile(RegexUserName)
+	reIpv4, _     = regexp.Compile(RegexIpv4)
+	reIpv6, _     = regexp.Compile(RegexIpv6)
+	reDomain, _   = regexp.Compile(RegexDomain)
+)
+
+func Mail(s string) bool {
+	return reMail.MatchString(s)
+}
+
+//UserName 帐号是否合法(字母开头，允许5-16字节，允许字母数字下划线)
+func UserName(s string) bool {
+	return reUserName.MatchString(s)
+}
+
+func Ipv4(s string) bool {
+	return reIpv4.MatchString(s)
+}
+
+func Ipv6(s string) bool {
+	return reIpv6.MatchString(s)
+}
+
+func Domain(s string) bool {
+	return reDomain.MatchString(s)
+}
+
+//Password 帐号是否合法(字母开头，允许5-16字节，允许字母数字下划线)
+func Password(str string, minLen, maxLen int) error {
+	var (
+		isUpper   = false
+		isLower   = false
+		isNumber  = false
+		isSpecial = false
+	)
+
+	if len(str) < minLen || len(str) > maxLen {
+		return fmt.Errorf("The password must contain uppercase and lowercase letters, numbers or punctuation, and must be %d-%d digits long. ", minLen, maxLen)
+	}
+
+	for _, s := range str {
+		switch {
+		case unicode.IsUpper(s):
+			isUpper = true
+		case unicode.IsLower(s):
+			isLower = true
+		case unicode.IsNumber(s):
+			isNumber = true
+		case unicode.IsPunct(s) || unicode.IsSymbol(s):
+			isSpecial = true
+		default:
+		}
+	}
+
+	if (isUpper && isLower) && (isNumber || isSpecial) {
+		return nil
+	}
+	return fmt.Errorf("The password must contain uppercase and lowercase letters, numbers or punctuation, and must be %d-%d digits long. ", minLen, maxLen)
+}
---
+++ validtor/validtor_test.go
@@ -0,0 +1,36 @@
+package validtor
+
+import "testing"
+import "github.com/stretchr/testify/assert"
+
+func TestMail(t *testing.T) {
+	assert.Equal(t, Mail("abc@xyz.com"), true, "this is mail")
+}
+
+func TestUserName(t *testing.T) {
+	assert.Equal(t, UserName("ABCd12"), true, "this is user name")
+	assert.NotEqual(t, UserName("1ABCd"), true, "this is user name")
+	assert.Equal(t, UserName("ABC_d"), true, "this is user name")
+	assert.Equal(t, UserName("ABC_d"), true, "this is user name")
+}
+
+func TestPassword(t *testing.T) {
+	assert.Equal(t, Password("ABCd12_", 6, 7), nil, "this is password")
+}
+
+func TestIpv4(t *testing.T) {
+	assert.Equal(t, Ipv4("127.0.0.255"), true, "this is not ipv4")
+	assert.Equal(t, Ipv4("0.0.0.255"), true, "this is not ipv4")
+}
+
+func TestDomain(t *testing.T) {
+	assert.Equal(t, Domain("abc.co"), true, "this is not domain")
+	assert.Equal(t, Domain("adl-xyz.abc.co"), true, "this is not domain")
+	assert.Equal(t, Domain("jd.com"), true, "this is not domain")
+}
+
+func BenchmarkIsMail(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Mail("abc@xyz.com")
+	}
+}
